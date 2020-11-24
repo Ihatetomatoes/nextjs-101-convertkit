6e7bd1a67a1072fefbc9dfc88be3d7fb4990c9c3
Petr Tichy <peter@ihatetomatoes.net>
2020-11-24 15:11:39 +1100

    layout and hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
 import Head from "next/head";
+import Hero from "./components/Hero";
+import Layout from "./components/Layout";
 
 export default function Home() {
   return (
-    <div className="mx-auto md:max-w-3xl md:px-4 min-h-screen">
+    <Layout>
       <Head>
         <title>Next.js 101</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="bg-white p-4">
-        <p>Hero component</p>
+        <Hero />
       </main>
-    </div>
+    </Layout>
   );
 }
