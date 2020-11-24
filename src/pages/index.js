7d9aec746e24384dee3714c30e386a6684c1d83a
Petr Tichy <peter@ihatetomatoes.net>
2020-11-24 12:05:56 +1100

    intro to tailwindCSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,14 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mx-auto md:max-w-3xl md:px-4 min-h-screen">
       <Head>
         <title>Next.js 101</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen flex flex-col justify-center">
-        <img
-          className="m-auto my-4 w-24 sm:w-auto"
-          alt="Ihatetomatoes"
-          src="/assets/img_logo.svg"
-        />
-        <h1 className="mx-auto text-4xl font-bold">Welcome to Next.js 101!</h1>
+      <main className="bg-white p-4">
+        <p>Hero component</p>
       </main>
     </div>
   );
