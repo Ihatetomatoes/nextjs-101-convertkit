3da5c7e2fd66725f96ee0eccf6e77672a7144903
Petr Tichy <peter@ihatetomatoes.net>
2020-12-07 12:34:00 +1100

    fixed path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: ["./src/pages/**/*.js", "./src/components/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
